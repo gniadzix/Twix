c591474f8cd99e4d183305c905dc8e69c115b23c
Adam Kacperczyk <adam.kacperczyk@student.wat.edu.pl>
2017-02-22 14:48:30 +0100

    Panel Dodaj wynik

--- Twix v2/Twixv2/Twixv2/Form1.Designer.cs
+++ Twix v2/Twixv2/Twixv2/Form1.Designer.cs
@@ -118,11 +118,12 @@
             this.textBoxZaktualizujDaneWyszukajPesel = new System.Windows.Forms.TextBox();
             this.labelZaktualizujDanePodajPesel = new System.Windows.Forms.Label();
             this.panelDodajWynik = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             buttonDodajPracownika = new System.Windows.Forms.Button();
             this.panelLogowanie.SuspendLayout();
             this.panelPanelPracownika.SuspendLayout();
@@ -262,6 +263,8 @@
             // 
             this.panelPanelPracownika.BackgroundImage = global::Twixv2.Properties.Resources.PanelPracownika_background;
             this.panelPanelPracownika.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelPanelPracownika.Controls.Add(this.panelDodajWynik);
+            this.panelPanelPracownika.Controls.Add(this.button4);
             this.panelPanelPracownika.Controls.Add(this.buttonPanelPracownikaZmienDaneUzytkownika);
             this.panelPanelPracownika.Controls.Add(this.buttonPanelPracownikaWyloguj);
             this.panelPanelPracownika.Controls.Add(this.buttonPanelPracownikaUsunUzytkownika);
@@ -286,7 +289,7 @@
             // 
             this.buttonPanelPracownikaWyloguj.BackgroundImage = global::Twixv2.Properties.Resources.button_wyloguj;
             this.buttonPanelPracownikaWyloguj.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.buttonPanelPracownikaWyloguj.Location = new System.Drawing.Point(18, 264);
+            this.buttonPanelPracownikaWyloguj.Location = new System.Drawing.Point(18, 294);
             this.buttonPanelPracownikaWyloguj.Name = "buttonPanelPracownikaWyloguj";
             this.buttonPanelPracownikaWyloguj.Size = new System.Drawing.Size(159, 39);
             this.buttonPanelPracownikaWyloguj.TabIndex = 2;
@@ -1141,49 +1144,10 @@
             this.panelDodajWynik.Controls.Add(this.textBox1);
             this.panelDodajWynik.Controls.Add(this.button2);
             this.panelDodajWynik.Controls.Add(this.button1);
-            this.panelDodajWynik.Location = new System.Drawing.Point(806, 2);
+            this.panelDodajWynik.Location = new System.Drawing.Point(0, 0);
             this.panelDodajWynik.Name = "panelDodajWynik";
             this.panelDodajWynik.Size = new System.Drawing.Size(800, 600);
             this.panelDodajWynik.TabIndex = 9;
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Pricedown Bl", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(324, 278);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(147, 38);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Dodaj";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Pricedown Bl", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button2.Location = new System.Drawing.Point(66, 153);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(128, 34);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Wyszukaj";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Pricedown Bl", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.textBox1.Location = new System.Drawing.Point(66, 100);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(156, 23);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "Nazwa użytkownika";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.Font = new System.Drawing.Font("Pricedown Bl", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(66, 215);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(156, 24);
-            this.comboBox1.TabIndex = 3;
-            this.comboBox1.Text = "Wybierz Broń";
             // 
             // button3
             // 
@@ -1196,23 +1160,74 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Pricedown Bl", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(66, 215);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(156, 24);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "Wybierz Broń";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Pricedown Bl", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(66, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Nazwa użytkownika";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Pricedown Bl", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(66, 153);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Wyszukaj";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Pricedown Bl", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(324, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dodaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Pricedown Bl", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.Location = new System.Drawing.Point(18, 240);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(159, 40);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Dodaj wynik";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // FormOknoGlowne
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Twixv2.Properties.Resources.Home_background;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(1587, 713);
-            this.Controls.Add(this.panelDodajWynik);
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.panelPanelPracownika);
             this.Controls.Add(this.buttonOknoGlowneZarejestruj);
             this.Controls.Add(this.buttonOknoGlowneWyjdz);
             this.Controls.Add(this.buttonOknoGlowneZaloguj);
+            this.Controls.Add(this.panelDodajUzytkownika);
             this.Controls.Add(this.panelRejestracja);
             this.Controls.Add(this.panelZaktualizujDane);
             this.Controls.Add(this.panelUsunUzytkownika);
             this.Controls.Add(this.panelLogowanie);
-            this.Controls.Add(this.panelPanelPracownika);
-            this.Controls.Add(this.panelDodajUzytkownika);
             this.Name = "FormOknoGlowne";
             this.Text = "Twix";
             this.panelLogowanie.ResumeLayout(false);
@@ -1329,6 +1344,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
